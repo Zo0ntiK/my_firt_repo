b45b383bf2d42662ece704a062b8d967d5adbda8
Zo0ntiK <matveyzotov0106@gmail.com>
2024-06-05 12:06:03 +0300

    висилица_v3

--- висилица.py
+++ висилица.py
@@ -4,14 +4,20 @@ def welcom_speech(t):
     output:return None,used as just built-in function print()
     
     '''
-    return t
+    print(f'''
+    добро пожадовать в игру - hangman 2000
+    ваша задача угадать загаданое слово за несколько попыток,
+    иначе вас ждёт расплата!
+    заганое слово состоит из {len(t)} букв {t}
+    ''')
+    
 
 def get_word(w):
     '''
     input: w - list with strings (words)
     output: for now: first element in list as string
     '''
-    return w[0]
+    return w[0]
 
 def start_template(w):
     '''
@@ -33,8 +39,39 @@ def list_to_string_convert(t):
     string = ''
     for i in t:
         string += i
-    return string
+    return string 
+        
+def list_to_string_convert_v2(t):
+    '''
+    input: t- template (list)
+    outputi s- list converted to string
+    '''
+    s = ''
+    return s.join(t)
+
+def user_input():
+    '''
+    output:return str,build-in input() function
+    '''
     
+    return input('ведите букву:')
+
+def build_template(t,w,g=''):
+    '''
+    input:t - temlate(list), w - word(string),g - guess(string)
+    output:t - template(list) with replaced characters in template
+                if character in word == guess:
+                    'sharacter'
+                else:
+                    '_'
+    '''
+    
+    for i in range(len(w)):
+        for u in w:
+             if g == w[i]:
+                 t[i] = w[i]
+    return t
+        
 def game():
     progress = True
     word = ['orange']
@@ -43,4 +80,9 @@ def game():
     word_in_play = get_word(word)
     template = star_template(word_in_play)
     welcom_speech(list_to_string_conwert(tempale))
+
+    while progress:
+        user_guess = user_input()
+        template = build_template(template,word_in_play,user_guess)
+
     
